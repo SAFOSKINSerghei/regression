1b5aef8d66202ab0640e295061176c8ded4f67ff
SAFOSKINSerghei <SAFOSKINSerghei@gmail.com>
2023-11-19 00:25:26 +0200

    uploading

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ st.set_page_config(layout='wide', page_title='Linear regression')
 
 st.markdown('<center><h1>Linear regression</h1></center>', unsafe_allow_html=True)
 datas = file_uploader_create("Select dataset file")
-
 try:
     x_cols = st.multiselect(
         'Select X(s):',
@@ -30,11 +29,13 @@ try:
     coefficients.set_index(np.array(['coefficients']), inplace=True)
     st.write(coefficients)
     coefficients = coefficients.iloc[0]
+    st.write(datas)
 
-    datas = datas[list(x_cols) + list(y_col)]
+    datas = datas[list(x_cols) + list([y_col])]
+
     datas['y_predicted'] = coefficients[0]
     for i in range(1, len(x_cols) + 1):
-        datas['y_predicted'] += datas['x' + str(i)] * coefficients[i]
+        datas['y_predicted'] += datas[datas.columns[i]] * coefficients[i]
     cols = st.columns([0.5, 0.5])
     with cols[0]:
         table_header = st.selectbox('Table head', [i for i in range(1, len(datas) + 1)])
@@ -43,5 +44,6 @@ try:
         st.write(str(model.score(x, y) * 100) + '%')
 
     st.markdown(datas.head(int(table_header)).style.hide(axis='index').to_html(), unsafe_allow_html=True)
-except:
-    pass
+
+except Exception as e:
+    st.write(e)
